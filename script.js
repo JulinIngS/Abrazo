da312c42259a6304a839dadc2d4b2ad21b78f3cb
JulinIngS <anaelmirey@gmail.com>
2023-01-02 23:23:27 -0500

    Abrazo

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiaMensaje,4500)
+
+function cambiaMensaje(){
+    document.getElementById("loading-message").innerText="Abrazo Enviado a la chica más guapa! Osea tú";
+}
+
+setTimeout(function () {
+    alert("Recibiste un abrazo virtual, Pero pronto recibiras uno presencial!! :D")
+},4550)
